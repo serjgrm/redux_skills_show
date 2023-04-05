fbd795d91ac13cc544129bfcd53e1b755f6ba6e5
Serhii <grammas56@gmail.com>
2023-04-05 16:06:19 +0300

    Layout with bootstrap settled

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
-
-function App() {
+import React from 'react';
+import './App.css'
+const App = () => {
   return (
-  1
+    <div className='app'>
+      <div  className='cash'> Cash </div>
+      <div className='jumbotron' style={{ display: 'flex' }} >
+        <button className='btn btn-primary'> Recharge</button>
+        <button className='btn btn-primary'> Withdraw from account</button>
+        <button className='btn btn-primary'> Add Customer </button>
+      </div>
+      <div className='customers'>
+        Customers
+      </div>
+    </div>
   );
-}
-
-export default App;
+};
+export default App;	
